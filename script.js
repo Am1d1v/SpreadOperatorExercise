bee91421eaae29cb3c2b03389d21b631b4232bf5
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-05-22 17:18:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+/*
+
+const buy = {
+    text: "Buy"
+}
+
+const button = {
+    width: 200,
+    color: "black"
+}
+
+const buyButton = {
+    ...buy,
+    ...button
+}
+console.table(buyButton);
+
+*/
+
+/*
+
+
+const buy = {
+    text: "Buy",
+    inner: {
+        text: "buyInner"
+    }
+}
+
+const button = {
+    width: 200,
+    color: "black",
+    inner: {
+        text: "Mutation Inner"
+    }
+}
+
+const buyButton = {
+    ...buy,
+    ...button
+}
+console.table(buyButton);
+console.log(buy);
+
+*/
+
